0feb2e49e22f2a3adffc8ec690cd8faff7918949
M. David Ramiro Barragan <dramirobarragan@gmail.com>
2023-07-02 18:55:26 +0200

    feat:end project

--- 11-cafeteria/gulpfile.js
+++ 11-cafeteria/gulpfile.js
@@ -5,50 +5,52 @@ const autoprefixer = require('autoprefixer');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
+const sourcemaps = require("gulp-sourcemaps");
+const cssnano = require("cssnano");
 
 function css(done) {
-  src('src/scss/app.scss')
-      .pipe(sass({
-        outputStyle: "compressed"
-      }))
-      .pipe(postcss([autoprefixer()]))
-      .pipe(dest('build/css'));
-  done();
+    src('src/scss/app.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'));
+    done();
 }
 
 function imagenes(done) {
-  src('src/img/**/*')
-      .pipe(imagemin({
-        optimizationLevel: 3
-      }))
-      .pipe(dest('build/img'));
+    src('src/img/**/*')
+        .pipe(imagemin({
+            optimizationLevel: 3
+        }))
+        .pipe(dest('build/img'));
 
-  done();
+    done();
 }
 
 function imageWebp(done) {
-  const opciones = {
-    quality: 50
-  }
-  src('src/img/**/*.{png,jpg}')
-      .pipe(webp(opciones))
-      .pipe(dest('build/img'));
-  done();
+    const opciones = {
+        quality: 50
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+    done();
 }
 
 function imageAvif(done) {
-  const opciones = {
-    quality: 50
-  }
-  src('src/img/**/*.{png,jpg}')
-      .pipe(avif(opciones))
-      .pipe(dest('build/img'));
-  done();
+    const opciones = {
+        quality: 50
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'));
+    done();
 }
 
 function dev() {
-  watch('src/scss/**/*.scss', css);
-  watch('src/img/**/*', imagenes);
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
 }
 
 
